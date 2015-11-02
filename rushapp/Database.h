c55929a0e357f13e43235603ceff6b5e46f0e8b3
brett meyer <bailderty@gmail.com>
2015-11-01 18:01:17 -0600

    Added event sort by date and fraternity sort by name methods to Database

--- rushapp/Database.h
+++ rushapp/Database.h
@@ -7,5 +7,17 @@
 
 @interface Database : NSObject
 
+//Fields
+@property (nonatomic) NSMutableArray * fraternityList;
+@property (nonatomic) NSMutableArray * eventList;
+@property (nonatomic) NSMutableArray * addressList;
+@property (nonatomic) NSMutableArray * favoritedList;
+
+//Initator
+-(instancetype) init;
+
+//Sort Arrays
+-(void)sortEventList;
+-(void)sortFraternityList;
 
 @end
--- rushapp/Database.m
+++ rushapp/Database.m
@@ -7,9 +7,34 @@
 
 #import <Foundation/Foundation.h>
 #import "Database.h"
+#import "Event.h"
 
 @implementation Database
 
+-(instancetype) init
+{
+    self = [super init];
+    
+    if(self)
+    {
+        self.fraternityList = [NSMutableArray alloc];
+        self.eventList = [NSMutableArray alloc];
+        self.addressList = [NSMutableArray alloc];
+        self.favoritedList = [NSMutableArray alloc];
+    }
+    return self;
+}
 
+-(void)sortEventList
+{
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"time" ascending:TRUE];
+    [self.eventList sortUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+}
+
+-(void)sortFraternityList
+{
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:TRUE];
+    [self.fraternityList sortUsingDescriptors:@[sortDescriptor]];
+}
 
 @end
