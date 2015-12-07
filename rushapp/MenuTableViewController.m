54c43ea96e854f6a7bf97e4fdfc73ce4b14cfeab
bailderty <bdmeyer2@wisc.edu>
2015-12-07 14:16:03 -0800

    Please don't break

--- rushapp/MenuTableViewController.m
+++ rushapp/MenuTableViewController.m
@@ -16,38 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.menuOptions = @[@"Event Feed", @"UW Fraternities", @"IFC Rush Information", @"Map Of Greek Houses"];
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self.navigationController pushViewController:self animated:YES];
+    self.navigationController.viewControllers = [[NSArray alloc] initWithObjects:self, nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [self.menuOptions count];
-}
-
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSString *identifier = @"cell";
-    
-    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    
-    if (cell == nil)
-    {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-    }
-    
-    cell.textLabel.text = self.menuOptions[indexPath.row];
-    return cell;
-}
 
 
 @end
