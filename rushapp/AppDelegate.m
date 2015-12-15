4d77c89e33ce9e1a5dfdf0e271ce3663974a4fe8
brett meyer <bailderty@gmail.com>
2015-12-15 16:55:30 -0600

    Test for github

--- rushapp/AppDelegate.m
+++ rushapp/AppDelegate.m
@@ -19,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     Database * sharedDatabase = [Database sharedDatabase];
+    // Singleton model
     return YES;
 }
 
